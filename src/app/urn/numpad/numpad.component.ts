76e52c4dcc200a94b6e2e7ab9be4eb4842463048
littlecouto17 <vinicius.coutinho.sou@gmail.com>
2018-11-19 23:31:20 -0200

    Numpad component generated

---
+++ src/app/urn/numpad/numpad.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-numpad',
+  templateUrl: './numpad.component.html',
+  styleUrls: ['./numpad.component.sass']
+})
+export class NumpadComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
