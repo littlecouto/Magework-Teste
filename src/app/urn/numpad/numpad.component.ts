e33ae20ab528a8b937f5f4c1e5c9bd8bd3da70f9
littlecouto17 <vinicius.coutinho.sou@gmail.com>
2018-11-21 05:53:32 -0200

    Numpad component adjusted with HTML and some logica

--- src/app/urn/numpad/numpad.component.ts
+++ src/app/urn/numpad/numpad.component.ts
@@ -1,15 +1,56 @@
-import { Component, OnInit } from '@angular/core';
+import { Input, Output, EventEmitter, Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-numpad',
+  selector: 'numpad',
   templateUrl: './numpad.component.html',
   styleUrls: ['./numpad.component.sass']
 })
 export class NumpadComponent implements OnInit {
 
-  constructor() { }
+  @Input() numbers : number;
+  @Output() getNumpadNumber = new EventEmitter<object>()
+  @Output() eventSaveVote = new EventEmitter<any>()
+  @Output() eventResetVote = new EventEmitter<any>()
+
+  private keyNumbers : number[];
+  private index = 0;
+
+  private isActive = true;
+
+  constructor() {}
 
   ngOnInit() {
+    this.keyNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+  }
+
+  resetVote(){
+    this.index = 0;
+    this.eventResetVote.emit();
+    this.isActive = true;
+  }
+
+  saveVote(){
+    this.eventSaveVote.emit();
+    this.resetVote();
+  }
+
+  setNumClicked(number: number){
+
+    if(!this.isActive)
+      return false;
+
+    this.getNumpadNumber.emit(
+      {
+        number: number,
+        index: this.index
+      }
+    );
+
+    this.index++;
+
+    this.numbers <= this.index &&
+      (this.isActive = false);
+
   }
 
 }
