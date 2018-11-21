23e587bd3d6af80d9e5f6b624935be2c6b04c035
littlecouto17 <vinicius.coutinho.sou@gmail.com>
2018-11-21 05:51:51 -0200

    Success modal generated

---
+++ src/app/urn/success-modal/success-modal.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'success-modal',
+  templateUrl: './success-modal.component.html',
+  styleUrls: ['./success-modal.component.sass']
+})
+export class SuccessModalComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
