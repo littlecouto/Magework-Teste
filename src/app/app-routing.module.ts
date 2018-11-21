f2f2e57b147b68346bbd37f554fdff20cc24d9fa
littlecouto17 <vinicius.coutinho.sou@gmail.com>
2018-11-21 05:50:41 -0200

    Making routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import { UrnComponent } from './urn/urn.component';
+
+const routes: Routes = [
+  { path: '', component: UrnComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
