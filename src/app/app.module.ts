516949904285ff363fb6c965f5b9deaed3336f58
littlecouto17 <vinicius.coutinho.sou@gmail.com>
2018-11-21 05:52:57 -0200

    Firebase configured and some modules added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,44 @@ import { UrnComponent } from './urn/urn.component';
 import { ScreenComponent } from './urn/screen/screen.component';
 import { NumpadComponent } from './urn/numpad/numpad.component';
 
+import { ForNumbersModule } from '../pipes/fornumbers/fornumbers.module';
+
+import {NgbModalModule} from '@ng-bootstrap/ng-bootstrap';
+
+import { AngularFireModule } from "@angular/fire";
+import { AngularFireAuth } from "@angular/fire/auth";
+import { AngularFirestore, AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireDatabase  } from '@angular/fire/database';
+import { SuccessModalComponent } from './urn/success-modal/success-modal.component';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyASGp6hEHS4y636XCO3_Tw9gDn0yOwgc1k",
+  authDomain: "magework-test.firebaseapp.com",
+  databaseURL: "https://magework-test.firebaseio.com",
+  projectId: "magework-test",
+  storageBucket: "magework-test.appspot.com",
+  messagingSenderId: "184163829947"
+};
+
+
 @NgModule({
   declarations: [
     AppComponent,
     UrnComponent,
     ScreenComponent,
-    NumpadComponent
+    NumpadComponent,
+    SuccessModalComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ForNumbersModule,
+    NgbModalModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFirestoreModule.enablePersistence()
   ],
-  providers: [],
+  entryComponents:[SuccessModalComponent],
+  providers: [AngularFireModule, AngularFirestore, AngularFireDatabase],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
